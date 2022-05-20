e6b2faf736a9466ab810dc1072b3acf17351d48a
LEONE <leonwahungu@gmail.com>
2022-05-20 15:15:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/implicitintentstutorial/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.implicitintentstutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnCall, btnCallFriend, btnWeb, btnMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCall = findViewById(R.id.btnCall);
+        btnCallFriend = findViewById(R.id.btnCallFriend);
+        btnWeb = findViewById(R.id.btnWeb);
+        btnMap = findViewById(R.id.btnMap);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+            }
+        });
+
+        btnCallFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 0728098670"));
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dkut.ac.ke"));
+                startActivity(intent);
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=Garden City Mall"));
+                startActivity(intent);
+            }
+        });
+    }
+}
